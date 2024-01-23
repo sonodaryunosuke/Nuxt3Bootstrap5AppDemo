77e9ee5ddcdf0d0331e426cf6ca3917ddfd5d234
sonodaryunosuke <146801708+sonodaryunosuke@users.noreply.github.com>
2024-01-23 04:38:38 +0000

    aaa

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,10 @@
 <template>
   <div>
-    <TrainingPlanList
-      :exercises="exercises"
-      :selectedExercise="selectedExercise"
-      @updateSelectedExercise="updateSelectedExercise"
-    />
-    <TrainingPlanDetails :exercise="selectedExercise" />
+    
   </div>
 </template>
 
 <script>
-import TrainingPlanList from './components/TrainingPlanList.vue';
-import TrainingPlanDetails from './components/TrainingPlanDetails.vue';
 
-export default {
-  data() {
-    return {
-      exercises: [
-        { id: 1, name: "大胸筋" },
-        { id: 2, name: "三頭筋" },
-        // 他のトレーニングデータ
-      ],
-      selectedExercise: null,
-    };
-  },
-  methods: {
-    updateSelectedExercise(exercise) {
-      this.selectedExercise = exercise;
-    },
-  },
-  components: {
-    TrainingPlanList,
-    TrainingPlanDetails,
-  },
-};
+
 </script>
